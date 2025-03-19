4262ceedeb57c165b2b6a9c861e2f2d5bcb5a77f
Do Hoang Hieu With Hangg <dohoanghieu7537@gmail.com>
2025-03-19 14:53:55 +0700

    update

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [name, setName] = useState("Hello");
+  const [age,setAge] = useState(19);
+  const [tao,setTao] = useState({
+    name: "Hello",
+    age: 20
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>{name}</Text>
+      <Text style={styles.text}>{age}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  text: {
+     fontSize: 30,
+     color: "red"
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    fontSize: 60,color:"red",
+    paddingTop: 20,
+    paddingHorizontal: 20
   },
 });
